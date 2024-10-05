59a2360b28df75be1e9dc9568840dc5b26fad0e1
BerkanGH <berkankeskin28@gmail.com>
2024-10-05 15:27:00 +0300

    odev

---
+++ DegiskenlerleUygulama/DegiskenlerleUygulama/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad;
+            string soyad;
+            string tcNo;
+            string telNo;
+            double ilkürünfiyatı;
+            double ikinciürünfiyatı;
+
+            Console.WriteLine("Merhaba adınızı yazınız:");
+            ad = Console.ReadLine();
+
+            Console.WriteLine("soyadınızı giriniz:");
+            soyad = Console.ReadLine();
+
+            Console.WriteLine("tc kimlik numaranızı yazınız"); 
+            tcNo = Console.ReadLine();
+            
+            Console.WriteLine("telefon numarasını yazınız");
+            telNo = Console.ReadLine();
+
+            Console.WriteLine("aldığınız ürünün fiyarını yazınız");
+            ilkürünfiyatı = Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
